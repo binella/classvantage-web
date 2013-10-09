a66b819aa8622d5c6d0c6a11beee248e145d25ff
Ata <ata.namvari@gmail.com>
2013-10-09 17:32:12 -0400

    adding rubrics to a page basics

--- app/scripts/controllers/page.js
+++ app/scripts/controllers/page.js
@@ -12,10 +12,22 @@ angular.module('classvantageApp')
 		};
 		
 		
+		$scope.rubricColDefs = [{ field: 'full_name', headerCellTemplate: '<a class="plus green" style="margin-left:-47px;position:relative;top:34px;" ng-click="newStudent();">Add a student</a>', width: 159},
+														{ field: 'rubric', headerCellTemplate: '<a class="plus green" style="margin-left:34px;position:relative;top:34px;" ng-click="newRubric()">Add your first rubric</a>', width: 265 }];
+		
 		// This can be done better no?
 		Page.get({id: $stateParams.page_id}, function (page, responseHeaders){
 			angular.extend($scope.page, page);
-
+			// Should this be done somehwere else?
+			angular.forEach(page.rubrics, function (rubric) {
+				$scope.rubricColDefs.splice($scope.rubricColDefs.length - 1, 0, 
+					{ field: 'WHAT_GOES_HERE', headerCellTemplate: '<a class="col-header" ui-sref="rubric({id:'+rubric.id+'})">'+rubric.title+'</a>', width: 116 });
+			});
+			if (page.rubrics.length > 0) {
+				var lastCol = $scope.rubricColDefs[$scope.rubricColDefs.length-1];
+				lastCol.width = 116;
+				lastCol.headerCellTemplate = '<a class="plus green" style="margin-left:41px;position:relative;top:34px;" ng-click="newRubric()"></a>';
+			};
 		}, function (httpResponse){
 			// Error getting page
 		});
@@ -24,22 +36,21 @@ angular.module('classvantageApp')
 		$scope.gridOptions = { 
 			data: 'page.students',
 			headerRowHeight: 90,
+			enableSorting: false,
 			plugins: [new ngGridFlexibleHeightPlugin()],
-			columnDefs: [{ field: 'full_name', headerCellTemplate: '<a class="plus green" style="margin-left:-47px;position:relative;top:25px;" ng-click="newStudent();">Add a student</a>', width: 148},
-									 { field: 'rubric', headerCellTemplate: '<a class="plus green" style="margin-left:30px;position:relative;top:25px;" href="">Add your first rubric</a>' }]
+			columnDefs: 'rubricColDefs'
+			//[{ field: 'full_name', headerCellTemplate: '<a class="plus green" style="margin-left:-47px;position:relative;top:25px;" ng-click="newStudent();">Add a student</a>', width: 148},
+			//						 { field: 'rubric', headerCellTemplate: '<a class="plus green" style="margin-left:30px;position:relative;top:25px;" ng-click="newRubric()">Add your first rubric</a>' }]
 		};
 		
 		
 		// New Rubric
 		$scope.newRubric = function () {
-			Rubric.save({},{}, function(rubric, postResponseHeader) {
+			Rubric.save({},{rubric: {page_id: $scope.page.id}}, function(rubric, postResponseHeader) {
 				// Success
 				Rubric.currentRubric = rubric;
 				$location.path('/rubrics/' + rubric.id);
-			}, function () {
-				// Error
-				alert('Error');
-			})
+			}, function () {	alert('Error creating rubric'); });
 		};
 		
 		// New Student
